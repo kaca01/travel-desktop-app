43c86cd02f6d16cd8636972455ef2a8bfcf84a6e
Hristina Adamovic <hristinacina@gmail.com>
2023-05-27 19:21:26 +0200

    imported MaterialDesign

---
+++ TravelApp/Database/Repository/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelApp.Database.Repository
+{
+    public abstract class BaseRepository
+    {
+        protected string _fileName;
+
+        public abstract void LoadFromFile();
+        public abstract void SaveToFile();
+    }
+}
