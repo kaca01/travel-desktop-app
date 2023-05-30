5697754e87d520b41963a8136628ae099909e865
Hristina Adamovic <hristinacina@gmail.com>
2023-05-30 22:10:12 +0200

    connected to in memory database

---
+++ TravelApp/Core/TravelContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelApp.Core.Model;
+
+namespace TravelApp.Core
+{
+    public class TravelContext : DbContext
+    {
+
+        protected override void OnConfiguring
+       (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "AuthorDb");
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- TravelApp/MainWindow.xaml.cs
+++ TravelApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TravelApp.Core;
+using TravelApp.Core.Model;
 
 namespace TravelApp
 {
@@ -22,6 +25,22 @@ namespace TravelApp
     {
         public MainWindow()
         {
+            using(var db = new TravelContext())
+{
+                Console.WriteLine("Adding some authors into database...");
+                User client = new User() { Name = "Roberto", Surname = "Bolano", Email = "roberto@gmail.com", Password = "pass1234", Role = Role.CLIENT };
+                User agent = new User() { Name = "Mario", Surname = "Giordani", Email = "mario@gmail.com", Password = "pass1234", Role = Role.AGENT };
+                db.Users.Add(client);
+                db.Users.Add(agent);
+                db.SaveChanges();
+            }
+
+            using (var context = new TravelContext())
+            {
+                var list = context.Users.ToList();
+                System.Diagnostics.Trace.WriteLine(list);
+            }
+
             InitializeComponent();
         }
     }
