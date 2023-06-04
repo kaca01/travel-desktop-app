f5652f7d2ef1a2b55747d057a3f11ea4705b0731
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-06-04 19:55:46 +0200

    Added commands to view model

--- TravelApp/DesktopHost/ViewModel/Component/Navigation/AgentNavigationViewModel.cs
+++ TravelApp/DesktopHost/ViewModel/Component/Navigation/AgentNavigationViewModel.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TravelApp.DesktopHost.Command.Navigation;
+using TravelApp.DesktopHost.Command.Navigation.Agent;
 
 namespace TravelApp.DesktopHost.ViewModel
 {
     class AgentNavigationViewModel : BaseViewModel
     {
+        public ICommand Trips { get; }
+        public ICommand Attractions { get; }
+        public ICommand StayAndEat { get; }
+        public ICommand SoldTrips { get; }
+        public ICommand Reservations { get; }
+        public ICommand Help { get; }
+        public ICommand LogOut { get; }
+
         public AgentNavigationViewModel() 
-        { 
+        {
+            Trips = new AgentTripsCommand();
+            Attractions = new AgentAttractionCommand();
+            StayAndEat = new AgentStayAndEatCommand(); 
+            SoldTrips = new AgentSoldTripsCommand();
+            Reservations = new AgentReservationsCommand();
+            Help = new AgentHelpCommand(); 
+            LogOut = new LogOutCommand();
         }
     }
 }
