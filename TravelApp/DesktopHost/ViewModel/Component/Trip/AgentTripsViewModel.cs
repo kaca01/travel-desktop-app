16c1777a0916fd0aa2dd68762390ee062b386940
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-06-04 20:15:14 +0200

    Created agent trips view
    And added navigation on it

---
+++ TravelApp/DesktopHost/ViewModel/Component/Trip/AgentTripsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelApp.DesktopHost.ViewModel
+{
+    class AgentTripsViewModel : BaseViewModel
+    {
+
+        public AgentTripsViewModel() 
+        {
+
+        }
+    }
+}
