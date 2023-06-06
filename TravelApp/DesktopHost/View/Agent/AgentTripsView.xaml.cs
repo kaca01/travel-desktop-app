e38d709f32562adcca29a51d56e703bfce4fadc1
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-06-06 21:38:08 +0200

    Finished the first row for agent all trips
    First row: title, search, new trip and sort by

--- TravelApp/DesktopHost/View/Agent/AgentTripsView.xaml.cs
+++ TravelApp/DesktopHost/View/Agent/AgentTripsView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TravelApp.DesktopHost.ViewModel;
 
 namespace TravelApp.DesktopHost.View
 {
@@ -23,6 +24,40 @@ namespace TravelApp.DesktopHost.View
         public AgentTripsView()
         {
             InitializeComponent();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double windowWidth = e.NewSize.Width;
+            double windowHeigth = e.NewSize.Height;
+
+            if (DataContext is AgentTripsViewModel viewModel)
+            {
+                if (windowWidth <= 1100)
+                {
+                    viewModel.TextFontSize = 42;
+                }
+                else if (windowWidth <= 1250)
+                {
+                    viewModel.TextFontSize = 46;
+                }
+                else
+                {
+                    viewModel.TextFontSize = 50;
+                }
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox)
+            {
+                if (comboBox.SelectedItem is string selectedItem)
+                {
+                    string selectedContent = selectedItem.ToString();
+                    // TODO : imeplement this
+                }
+            }
         }
     }
 }
--- TravelApp/DesktopHost/ViewModel/Component/Agent/AgentTripsViewModel.cs
+++ TravelApp/DesktopHost/ViewModel/Component/Agent/AgentTripsViewModel.cs
@@ -12,9 +12,39 @@ namespace TravelApp.DesktopHost.ViewModel
     {
         private string _windowSize;
 
+        private double _textFontSize;
+
+        private List<string> _sortCriteria;
+
         public AgentNavigationViewModel Navigation { get; set; }
 
         public ICommand DisplayWindowSize { get; }
+
+        public List<string> SortCriteria
+        {
+            get { return _sortCriteria; }
+            set 
+            { 
+                if (value != _sortCriteria)
+                {
+                    _sortCriteria = value;
+                    OnPropertyChanged(nameof(SortCriteria));
+                }
+            }
+        }
+
+        public double TextFontSize
+        {
+            get { return _textFontSize; }
+            set
+            {
+                if (_textFontSize != value)
+                {
+                    _textFontSize = value;
+                    OnPropertyChanged(nameof(TextFontSize));
+                }
+            }
+        }
 
         public string WindowSize
         {
@@ -29,6 +59,14 @@ namespace TravelApp.DesktopHost.ViewModel
         public AgentTripsViewModel() 
         {
             _windowSize = "Dimenzije prozora";
+            _textFontSize = 50;
+            _sortCriteria = new List<string>();
+            _sortCriteria.Add("All");
+            _sortCriteria.Add("Trip name");
+            _sortCriteria.Add("Departure location");
+            _sortCriteria.Add("Destination location");
+            _sortCriteria.Add("One day trip");
+            _sortCriteria.Add("Multi days trip");
             Navigation = new AgentNavigationViewModel();
             DisplayWindowSize = new DisplayWidnowSizeCommand(this);
         }
