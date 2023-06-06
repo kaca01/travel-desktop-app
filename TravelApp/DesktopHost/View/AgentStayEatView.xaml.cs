73d4134ef4aa4471957bd2b64fbc8206495a84f0
savic-a <anastasijas557@gmail.com>
2023-06-06 13:37:26 +0200

    Designed stay and eat table for agent

---
+++ TravelApp/DesktopHost/View/AgentStayEatView.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TravelApp.DesktopHost.ViewModel;
+
+namespace TravelApp.DesktopHost.View
+{
+    /// <summary>
+    /// Interaction logic for AgentStayEatView.xaml
+    /// </summary>
+    public partial class AgentStayEatView : UserControl
+    {
+        public AgentStayEatView()
+        {
+            InitializeComponent();
+            DataContext = new ClientStayEatViewModel();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            string url = e.Uri.AbsoluteUri;
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            };
+
+            Process.Start(psi);
+            e.Handled = true;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double windowWidth = e.NewSize.Width;
+            double windowHeigth = e.NewSize.Height;
+
+            // Adjust the font size based on the window width
+            ClientStayEatViewModel viewModel = (ClientStayEatViewModel)DataContext;
+            if (windowWidth <= 1200 || windowHeigth <= 700)
+            {
+                viewModel.TextFontSize = 40;
+                viewModel.Width = 300;
+            }
+            else
+            {
+                viewModel.TextFontSize = 60;
+                viewModel.Width = 439;
+            }
+        }
+    }
+}
