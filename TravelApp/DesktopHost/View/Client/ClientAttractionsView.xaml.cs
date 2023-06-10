bd25da8309cee15c6f3cb45f0a1b4d11231d6320
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-06-10 02:47:23 +0200

    Created client attractions view model and view

---
+++ TravelApp/DesktopHost/View/Client/ClientAttractionsView.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TravelApp.DesktopHost.ViewModel;
+
+namespace TravelApp.DesktopHost.View
+{
+    /// <summary>
+    /// Interaction logic for ClientAttractionsView.xaml
+    /// </summary>
+    public partial class ClientAttractionsView : UserControl
+    {
+        public ClientAttractionsView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double windowWidth = e.NewSize.Width;
+
+            if (DataContext is ClientAttractionsViewModel viewModel)
+            {
+                if (windowWidth <= 1000)
+                {
+                    viewModel.FieldsWidth = 150;
+                    viewModel.TextFontSize = 30;
+
+                }
+                else if (windowWidth <= 1100)
+                {
+                    viewModel.FieldsWidth = 170;
+                    viewModel.TextFontSize = 35;
+                }
+                else if (windowWidth <= 1250)
+                {
+                    viewModel.FieldsWidth = 200;
+                    viewModel.TextFontSize = 46;
+                }
+                else
+                {
+                    viewModel.FieldsWidth = 280;
+                    viewModel.TextFontSize = 50;
+                }
+            }
+        }
+    }
+}
