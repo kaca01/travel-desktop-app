217c9eeaf67687562c6f1e0067914cf2dabbf3b2
savic-a <anastasijas557@gmail.com>
2023-06-12 02:51:05 +0200

    Created class for help

--- TravelApp/MainWindow.xaml.cs
+++ TravelApp/MainWindow.xaml.cs
@@ -41,5 +41,18 @@ namespace TravelApp
             }
             InitializeComponent();
         }
+
+        public void doThings(string param) 
+        { }
+
+        public void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            IInputElement focusedControl = FocusManager.GetFocusedElement(Application.Current.Windows[0]);
+            if (focusedControl is DependencyObject)
+            {
+                string str = HelpProvider.GetHelpKey((DependencyObject)focusedControl);
+                HelpProvider.ShowHelp(str, this);
+            }
+        }
     }
 }
