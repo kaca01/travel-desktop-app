e50a6d54a545ca808c95e6104667959e800e6d09
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-06-12 19:18:56 +0200

    Bugfix with trips description - agent

--- TravelApp/DesktopHost/ViewModel/Component/Agent/AgentTripsViewModel.cs
+++ TravelApp/DesktopHost/ViewModel/Component/Agent/AgentTripsViewModel.cs
@@ -177,8 +177,18 @@ namespace TravelApp.DesktopHost.ViewModel
             List<Trip> trips = new List<Trip>();
             foreach (Trip trip in _trips)
             {
-                trip.Description = trip.Description.Substring(0, Math.Min(trip.Description.Length, 120)) + "...";
-                trips.Add(trip);
+                Trip t = new Trip();
+                t.Id = trip.Id;
+                t.IsDeleted = trip.IsDeleted;
+                t.StartDate = trip.StartDate;
+                t.EndDate = trip.EndDate;
+                t.Price = trip.Price;
+                t.Name = trip.Name;
+                t.Departure = trip.Departure;
+                t.Departure = trip.Destination;
+                t.Image = trip.Image;
+                t.Description = trip.Description.Substring(0, Math.Min(trip.Description.Length, 120)) + "...";
+                trips.Add(t);
             }
 
             return trips;
