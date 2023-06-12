68422b5d746a43e8fe75de0836e84ea4720acdf9
kaca01 <96604086+kaca01@users.noreply.github.com>
2023-06-12 02:31:14 +0200

    Created client view model and view

---
+++ TravelApp/DesktopHost/ViewModel/Component/Client/ClientTripDetailsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelApp.DesktopHost.ViewModel
+{
+    public class ClientTripDetailsViewModel : BaseViewModel
+    {
+        private double _textFontSize;
+
+        private double _fieldsWidth;
+
+        public double TextFontSize
+        {
+            get { return _textFontSize; }
+            set
+            {
+                if (_textFontSize != value)
+                {
+                    _textFontSize = value;
+                    OnPropertyChanged(nameof(TextFontSize));
+                }
+            }
+        }
+
+        public double FieldsWidth
+        {
+            get => _fieldsWidth;
+            set
+            {
+                _fieldsWidth = value;
+                OnPropertyChanged(nameof(FieldsWidth));
+            }
+        }
+    }
+}
