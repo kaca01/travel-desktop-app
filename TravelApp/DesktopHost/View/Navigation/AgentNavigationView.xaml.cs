da0bca0992b3a994a394a4306a3894f9f82eaf57
Hristina Adamovic <hristinacina@gmail.com>
2023-06-13 05:01:59 +0200

    implemented shortcuts for agent navigation

--- TravelApp/DesktopHost/View/Navigation/AgentNavigationView.xaml.cs
+++ TravelApp/DesktopHost/View/Navigation/AgentNavigationView.xaml.cs
@@ -30,7 +30,120 @@ namespace TravelApp.DesktopHost.View
         {
             InitializeComponent();
             SetHelpKey(null, null);
-            Help.Focus();
+        }
+
+        private void TabControl_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Tab && Keyboard.Modifiers == ModifierKeys.None)
+            {
+                // Get the currently focused element
+                var focusedElement = Keyboard.FocusedElement as UIElement;
+
+                // Find the parent TabItem of the focused element
+                var tabItem = FindParentTabItem(focusedElement);
+
+                // Find the parent TabControl of the focused element
+                var tabControl = FindParentTabControl(tabItem);
+
+                // Get the list of TabItems within the TabControl
+                var tabItems = tabControl.Items.OfType<TabItem>().ToList();
+
+                // Find the index of the currently focused TabItem
+                var focusedIndex = tabItems.IndexOf(tabItem);
+
+                // Determine the index of the next TabItem based on the Tab key pressed
+                var nextIndex = Keyboard.Modifiers == ModifierKeys.Shift ? focusedIndex - 1 : focusedIndex + 1;
+
+                // Make sure the next index stays within the range of TabItems
+                if (nextIndex < 0)
+                    nextIndex = tabItems.Count - 1;
+                else if (nextIndex >= tabItems.Count)
+                    nextIndex = 0;
+
+                // Get the next TabItem
+                var nextTabItem = tabItems[nextIndex];
+
+                // Find the first focusable element within the next TabItem's content
+                var nextFocusableElement = FindFirstFocusableElement(nextTabItem);
+
+                // Set the focus to the next focusable element
+                nextFocusableElement?.Focus();
+
+                // Mark the event as handled
+                e.Handled = true;
+
+            }
+            else if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                if (SelectedIndex == 0)
+                    BaseViewModel.AgentNavigationViewModel.Trips.Execute(null);
+                else if (SelectedIndex == 1)
+                    BaseViewModel.AgentNavigationViewModel.Attractions.Execute(null);
+                else if (SelectedIndex == 2)
+                    BaseViewModel.AgentNavigationViewModel.StayAndEat.Execute(null);
+                else if (SelectedIndex == 3)
+                    BaseViewModel.AgentNavigationViewModel.SoldTrips.Execute(null);
+                else if (SelectedIndex == 4)
+                    BaseViewModel.AgentNavigationViewModel.Reservations.Execute(null);
+                else if (SelectedIndex == 5)
+                    BaseViewModel.AgentNavigationViewModel.Help.Execute(null);
+                else if (SelectedIndex == 6)
+                    BaseViewModel.AgentNavigationViewModel.LogOut.Execute(null);
+                // Mark the event as handled
+                e.Handled = true;
+            }
+        }
+
+        private TabItem FindParentTabItem(UIElement element)
+        {
+            if (element == null)
+                return null;
+
+            var parent = VisualTreeHelper.GetParent(element);
+
+            while (parent != null && !(parent is TabItem))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent as TabItem;
+        }
+
+        private TabControl FindParentTabControl(UIElement element)
+        {
+            if (element == null)
+                return null;
+
+            var parent = VisualTreeHelper.GetParent(element);
+
+            while (parent != null && !(parent is TabControl))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return parent as TabControl;
+        }
+
+        private UIElement FindFirstFocusableElement(DependencyObject element)
+        {
+            if (element == null)
+                return null;
+
+            if (element is UIElement uiElement && uiElement.Focusable)
+                return uiElement;
+
+            int childCount = VisualTreeHelper.GetChildrenCount(element);
+
+            for (int i = 0; i < childCount; i++)
+            {
+                var childElement = VisualTreeHelper.GetChild(element, i);
+                var result = FindFirstFocusableElement(childElement);
+
+                if (result != null)
+                    return result;
+            }
+
+            return null;
         }
 
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
